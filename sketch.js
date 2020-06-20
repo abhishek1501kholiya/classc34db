c26edab6dac7bf7189c83bac68f573000c8fd8f5
abhishek1501kholiya <abhishekkholiya15012007@gmai.com>
2020-06-21 00:15:10 +0530

    hello

--- sketch.js
+++ sketch.js
@@ -1,29 +1,50 @@
+var database;
+
 var ball;
+var dbposition;
 
 function setup(){
     createCanvas(500,500);
+    database = firebase.database();
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    var ballposition = database.ref('ball/position');
+    ballposition.on("value",read,error);
+
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+     database.ref('ball/position').set({
+         'x': dbposition.x + x ,
+         'y':  dbposition.y + y
+        
+     })
+   
+    }
+function read(data){
+  dbposition = data.val();
+  ball.x = dbposition.x;
+  ball.y = dbposition.y;
+  console.log(dbposition);
+}
+
+function error(){
+     console.log("error");
 }
